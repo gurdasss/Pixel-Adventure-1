e5c964bf14aa7819d844c30f8957d171ea688549
Gurdas Satwani <gurdassatwani1@gmail.com>
2024-09-29 18:52:23 +0530

    need to rethink about how to desgine this game

---
+++ scripts/collectables.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _on_body_entered(body: Node2D) -> void:
+	animated_sprite.play("collected")
+	
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	queue_free()
