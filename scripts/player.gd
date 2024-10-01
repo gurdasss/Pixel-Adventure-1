9987b141c7ea880f4ab78a90a22cc62ce4c327d4
Gurdas Satwani <gurdassatwani1@gmail.com>
2024-10-01 06:19:16 +0530

    able to generate random skin whenever the user restart the game

---
+++ scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var skin_manager: Node2D = $SkinManager
+var currentSkin: AnimatedSprite2D
+
+
+# the below _ready function will get called once
+# every child of this node's ready gets executed.
+func _ready() -> void:
+	# store all the childrens of skin_manager in animation list
+	# and then pick a random skin from that array
+	# every time player node enters the scene tree.
+	var animationList: Array[Node] = skin_manager.get_children()
+	currentSkin = animationList.pick_random()
+	currentSkin.visible = true
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
