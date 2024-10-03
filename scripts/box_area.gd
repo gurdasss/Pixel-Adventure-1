8bd09a566cbfca24d5bbd2cb11cad047dafe6dc7
Gurdas Satwani <gurdassatwani1@gmail.com>
2024-10-03 06:23:35 +0530

    cleaned up few redundant nodes

--- scripts/box_area.gd
+++ scripts/box_area.gd
@@ -1,18 +1,26 @@
 extends Area2D
 
 var animatedSprite: AnimatedSprite2D = null
-var particle: CPUParticles2D = null
+@onready var cpu_particles: CPUParticles2D = $CPUParticles2D
+
+@export var texturePath: String = ""
+@export var initialVelocityMin: float = 100.00
+@export var initialVelocityMax: float = 150.00
 
 # number of hits different types of box can take
 @export var numberOfHits: int = 1
 
 func _ready() -> void:
-	# get the child of each instance
-	# AnimatedSprite2D (second last child) 
-	animatedSprite = get_child(-2)
+	# get the last node from each instance of this node.
+	# AnimatedSprite2D (last child)
+	animatedSprite = get_child(-1)
 	
-	# CPUParticles2D (last child)
-	particle = get_child(-1)
+	# load the specified texturePath during runtime
+	cpu_particles.texture = load(texturePath)
+	
+	cpu_particles.initial_velocity_min = initialVelocityMin
+	cpu_particles.initial_velocity_max = initialVelocityMax
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	if numberOfHits > 0:
@@ -22,10 +30,10 @@ func _on_body_entered(body: Node2D) -> void:
 	# numberOfHits is zero or negative
 	if is_instance_valid(animatedSprite) and not numberOfHits:
 		animatedSprite.queue_free()
-		particle.emitting = true # start emitting particles
+		cpu_particles.emitting = true # start emitting particles
 		# connect with CPUParticles2D's finished signal
 		# and then call queue_free from self.
-		particle.connect("finished", Callable(self, "queue_free"))
+		cpu_particles.connect("finished", Callable(self, "queue_free"))
 
 func _on_body_exited(body: Node2D) -> void:
 	if is_instance_valid(animatedSprite):
