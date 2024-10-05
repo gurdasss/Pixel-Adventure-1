9aaaf8ca8b45553c3cb6aff9f93c0858ff9859ef
Gurdas Satwani <gurdassatwani1@gmail.com>
2024-10-05 16:17:11 +0530

    finally created level 1

---
+++ scripts/level_manager.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var background: TileMapLayer = $Background
+@onready var player: CharacterBody2D = $Player
+
+@export var playerStartingPosition: Vector2 = Vector2(0,0)
+
+const BG_TEXTURE = [
+	"res://assets/Background/Blue.png",
+	"res://assets/Background/Brown.png",
+	"res://assets/Background/Gray.png",
+	"res://assets/Background/Green.png",
+	"res://assets/Background/Pink.png",
+	"res://assets/Background/Purple.png",
+	"res://assets/Background/Yellow.png"
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player.position = playerStartingPosition
+	background.tile_set.get_source(1).texture = load(BG_TEXTURE.pick_random())
