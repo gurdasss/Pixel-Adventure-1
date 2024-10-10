36070460e45236bdc1e9fb903d4785424e39c8ae
Gurdas Satwani <gurdassatwani1@gmail.com>
2024-10-10 06:45:15 +0530

    finally I was able to add double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var flag: bool = true
 
 var currentSkin: AnimatedSprite2D
 
@@ -19,15 +20,23 @@ func _ready() -> void:
 	add_child(currentSkin)
 	
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+		if Input.is_action_just_pressed("ui_accept") and flag:
+			currentSkin.play("double jump")
+			velocity.y = -250
+			flag = false
+		
+		if not currentSkin.is_playing():
+			currentSkin.play("idle")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		flag = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
